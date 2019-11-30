0852be49cfe9d20b5e0efe1bc531e0220aaf6a7e
codedrift <peter.moesenthin@gmail.com>
2019-12-01 00:02:28 +0100

    feat: disable search default

--- theme/header.php
+++ theme/header.php
@@ -39,7 +39,7 @@
 					<?php
 
 					// Check whether the header search is activated in the customizer.
-					$enable_header_search = get_theme_mod( 'enable_header_search', true );
+					$enable_header_search = get_theme_mod( 'enable_header_search', false );
 
 					if ( true === $enable_header_search ) {
 
